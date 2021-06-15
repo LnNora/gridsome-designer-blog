39ae8641552d85e59ab2c1a3305692b8fef7b922
Nan Luo <1410314963@qq.com>
2021-06-15 17:54:32 +0800

    complete

---
+++ src/pages/Index.vue
@@ -0,0 +1,58 @@
+<template>
+  <Layout>
+    <div class="container">
+      <Hero :basicInfo="$page.basicInfo.edges[0].node"/>
+      <ProjectsGrid :projects="$page.projects.edges" />
+    </div>
+    <LatestJournals :journals="$page.journals.edges" />
+  </Layout>
+</template>
+
+<page-query>
+query Posts {
+	projects: allStrapiProject {
+    edges {
+      node {
+        id
+        updated_at
+        title
+        categories {
+          title
+        }
+        thumbnail {
+          url
+        }
+      }
+    }
+  }
+  basicInfo: allStrapiGeneral {
+    edges {
+      node {
+        title
+        subtitle
+      }
+    }
+  }
+  journals: allStrapiJournal(perPage: 4) {
+    edges {
+      node {
+        id
+        title
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import Hero from "@/components/Hero"
+import ProjectsGrid from "@/components/ProjectsGrid"
+import LatestJournals from "@/components/LatestJournals"
+export default {
+  components: {
+    Hero,
+    ProjectsGrid,
+    LatestJournals
+  }
+}
+</script>
